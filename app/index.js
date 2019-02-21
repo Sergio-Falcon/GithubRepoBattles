0cf2373ba82c86cfaa35fc7d20f85b3af44eb6d4
Sergio Falcon <SergioJFalcon@gmail.com>
2019-02-20 19:17:33 -0600

    Github Battles first commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+
+//	3 properties we usually care about
+	//	state
+	//	lifecycle event
+	//	UI
+
+
+//Component definition
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
